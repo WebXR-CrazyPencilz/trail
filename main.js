5e99ed88602718fc12c35ae69d4c488651b56c17
WebXR-CrazyPencilz <3dactz@gmail.com>
2026-05-21 11:42:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -70,12 +70,12 @@ const hotspots = {
   kidsbedroomcorridor: [
     { target: 'livingtobedroom',        position: [-5.0,  -2.2, -0.2 ] },
     { target: 'kidsbedroom',            position: [ 5.0,  -2.2, -2. ] },
-    { target: 'kidsbedroomtoilet',      position: [-2., -2.2, -2.50 ] }
+    { target: 'kidsbedroomtoilet',      position: [-2.4, -2.2, -2.0 ] }
   ],
 
   // ── Kids Bedroom ───────────────────────────────────────────
   kidsbedroom: [
-    { target: 'kidsbedroomcorridor',    position: [-4.8,  -2.2,  3.2 ] }
+    { target: 'kidsbedroomcorridor',    position: [-4.8,  -2.2,  3. ] }
   ],
 
   // ── Kids Toilet ────────────────────────────────────────────
